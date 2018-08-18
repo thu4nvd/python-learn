717170497ae31efdbb7a8137bd63210bca294952
Thuan VU <thuanvd@outlook.com>
2018-08-18 22:31:03 +0700

    deque

--- ex_argparse.py
+++ ex_argparse.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#example of argparse modul
+#example of argparse module
 
 
 import argparse
---
+++ example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#This is only example file, for silly things
+
+import update_readme
+
+
+def main():
+    print(update_readme.__doc__)
+
+
+if __name__ == "__main__":
+    main()
